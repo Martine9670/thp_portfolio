a9643939259997966f3bcfcb88cc2eafd14f739a
Martine9670 <pinnamartine@free.fr>
2026-02-09 10:52:02 +0100

    rmv words in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ export default function Home({ theme }) {
       {/* LISTE DES TRAVAUX */}
       <section className="projects">
         <p>
-          Vous pourrez trouver ci-dessous la liste de mes projets. J'en ai réalisé certains
+          Vous pourrez trouver la liste de mes projets. J'en ai réalisé certains
           lors de ma formation chez The Hacking Project, et d'autres sont des projets personnels.
         </p>
 
